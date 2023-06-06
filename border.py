91fff2337f343df9d22d99e30f2170b31df1cae2
arryadali <arryadali@gmail.com>
2023-06-07 00:25:50 +0700

    Second Commit

--- border.py
+++ border.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-path = r"C:\pemprog\python\ProjectFaceDetector\picture\myluv.jpg"
+path = r"C:\pemprog\python\ProjectFaceDetector\picture\arrya.png"
 
 img = cv.imread(path, cv.IMREAD_COLOR)
 cv.copyMakeBorder(img, 100, 100, 100, 100, cv.BORDER_REFLECT)
--- load_image2.py
+++ load_image2.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-path = r"C:\pemprog\python\ProjectFaceDetector\picture\myluv.jpg"
+path = r"C:\pemprog\python\ProjectFaceDetector\picture\arrya.png"
 
 #read the image
 img = cv.imread(path, cv.IMREAD_COLOR)
